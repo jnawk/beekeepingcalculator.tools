c7edc5818a2c401fd461d56b189f7a447994834c
Philip Dowie <philip@jnawk.nz>
2022-05-04 20:45:41 +1200

    initial infra commit

--- lib/beekeepingcalculators.com-stack.ts
+++ lib/beekeepingcalculators.com-stack.ts
@@ -1,16 +1,35 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import {   
+  aws_cloudfront as cloudfront,
+  aws_cloudfront_origins as origins,
+  aws_s3 as s3,
+  aws_s3_deployment as s3deploy,
+} from 'aws-cdk-lib';
 
 export class BeekeepingcalculatorsComStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {versioned: true})
+    
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket)
+      },
+      defaultRootObject: "index.html"
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BeekeepingcalculatorsComQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [ s3deploy.Source.asset("./website")],
+      destinationBucket: websiteBucket,
+      distribution, 
+      distributionPaths: ["/*"],
+    })
+
+    new cdk.CfnOutput(this, "DistributionDomainName", {
+      value: distribution.domainName
+    })
   }
 }
